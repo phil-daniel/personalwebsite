77780505a1f706ae1a6d5a04e75d2ee5cdf13e66
Phillip Daniel <fv22283@bristol.ac.uk>
2023-09-20 22:08:14 +0100

    Added copyright message

--- personalwebsite/src/App.js
+++ personalwebsite/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navbar from "./components/Navbar/navbar";
+import Copyright from "./components/Copyright/copyright"
 import Home from "./pages/home";
 import Projects from "./pages/projects";
 import { Route, Routes } from "react-router-dom";
@@ -15,6 +16,7 @@ function App() {
           <Route path="/cv" />
         </Routes>
       </div>
+      <Copyright />
     </>
   )
 }
