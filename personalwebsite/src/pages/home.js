a5d1c785bd91a16c4ca28690e6e98cde86297d9b
Phillip Daniel <fv22283@bristol.ac.uk>
2023-09-20 22:19:59 +0100

    Began adding information to the homepage

--- personalwebsite/src/pages/home.js
+++ personalwebsite/src/pages/home.js
@@ -2,6 +2,12 @@ import React from 'react';
 
 export default function Home() {
     return <>
-        Homepage
+        <h1>Hello, my name is Phillip.</h1>
+        <h2>I am a Second Year student at the University of Bristol, studying Computer Science (Bsc), graduating 2025.</h2>
+        <h2>I can use an assortment of languages, including C, Python, Haskell, Lua and Javascript.</h2>
+        <h2>In the future I also plan to learn C++ and C#</h2>
+        <h2>I am constantly looking for ways to improve my programming skills and am also actively looking for internships that could help me nurture my interests through experience in the enterprise work, if you have any possibilities, please contact me below.</h2>
+        <h3>Outside of Computer Science I enjoy travelling, with some of my favourite places being Shenzhen, Hongkong & Prague.</h3>
     </>
 }
+//Link to travel photos?
