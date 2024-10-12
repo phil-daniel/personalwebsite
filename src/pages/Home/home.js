c144bf34fd8c5734c3f2c0481dcfe325de096aba
Phillip Daniel <fv22283@bristol.ac.uk>
2024-10-12 12:51:55 +0100

    Slight updates to test autodeploying

--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -7,7 +7,7 @@ export default function Home() {
     document.title = 'Phil Daniel | Home';
     return <div class="Home-Content">
         <h1 className="hello">Hi, welcome to my website. I'm Phillip.</h1>
-        <p className="main-text">I am a Second Year student at the <a href="https://www.bristol.ac.uk" target="_blank" rel="noopener noreferrer" className="link">University of Bristol</a>, studying Computer Science (BSc), graduating in 2025.
+        <p className="main-text">I am a Third Year student at the <a href="https://www.bristol.ac.uk" target="_blank" rel="noopener noreferrer" className="link">University of Bristol</a>, studying Computer Science (BSc), graduating in 2025.
              I have knowedge in an assortment of language, including C, Python, Haskell, Lua, Go and others,
              and I am currently learning C++. I am constantly looking for ways to improve my programming skills and am also actively looking
              for a summer internship which could help me nurture my interests through experience in the enterprise work,
